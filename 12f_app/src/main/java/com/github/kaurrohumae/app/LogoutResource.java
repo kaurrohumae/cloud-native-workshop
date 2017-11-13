2a683e7c793326071859a5497c4268d46ef37a79
Kaur Rohumäe <Kaur.Rohumae@smit.ee>
2017-11-13 14:33:30 +0200

    Step 6. fix travis.

---
+++ 12f_app/src/main/java/com/github/kaurrohumae/app/LogoutResource.java
@@ -0,0 +1,17 @@
+package com.github.kaurrohumae.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class LogoutResource {
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(HttpSession session) {
+
+        session.invalidate();
+        return "Logout done!";
+    }
+}
