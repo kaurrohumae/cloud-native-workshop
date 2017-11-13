8caa9092ef28f236fa90ad0200c5d3d880fff561
Kaur Rohumäe <Kaur.Rohumae@smit.ee>
2017-11-13 15:35:03 +0200

    add logback

--- 12f_app/src/main/java/com/github/kaurrohumae/app/StockResource.java
+++ 12f_app/src/main/java/com/github/kaurrohumae/app/StockResource.java
@@ -1,6 +1,8 @@
 package com.github.kaurrohumae.app;
 
 import com.github.kaurrohumae.app.domain.model.StockItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -23,6 +25,8 @@ public class StockResource {
     @Autowired
     private StockItemRepository stockItemRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(StockResource.class);
+
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
     public String ping() {
         return "Ping";
@@ -35,15 +39,20 @@ public class StockResource {
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public StockItem stockItem(@PathVariable("id") Long id, HttpServletResponse response) {
+        logger.info("Starting search of stock item(id={}) search", id);
+
         StockItem stockItem = stockItemRepository.findOne(id);
 
         if (stockItem == null) {
+            logger.info("Stock item(id={}) has not been found", id);
             response.setStatus(HttpStatus.NOT_FOUND.value());
             return null;
         }
 
+        logger.info("Finishing search of stock item(id={}) search", id);
         return stockItem;
     }
+
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public StockItem storeInStock(@RequestBody StockItem stockItem, HttpServletResponse response) {
